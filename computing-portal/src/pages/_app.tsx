9bebcc954d774b6a93bd0a59eac0205f6ad46faa
Joe tay <tay.joe@gmail.com>
2026-01-07 22:22:33 +0800

    Fix spreadsheet: Add FortuneSheet CSS import

--- computing-portal/src/pages/_app.tsx
+++ computing-portal/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import '@fortune-sheet/react/dist/index.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
